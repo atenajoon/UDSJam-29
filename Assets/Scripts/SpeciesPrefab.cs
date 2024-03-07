61387700ff0c99c0982a097d52a23838abcec051
Atena Razm <atenarazm@gmail.com>
2024-03-07 12:28:12 -0800

    evolve_0 prefabs created

--- Assets/Scripts/SpeciesPrefab.cs
+++ Assets/Scripts/SpeciesPrefab.cs
@@ -16,16 +16,22 @@ public class SpeciesPrefab : MonoBehaviour
         {
             string collidedTag = collision.gameObject.tag;
             //  if the 2 collided species are the same
-            if(gameObject.CompareTag(collidedTag))
+            if (gameObject.CompareTag(collidedTag))
             {
-                // combine them to evolve
-                Debug.Log("collidedTag: " + collidedTag);
-                //  explode the other ones
+
+                // Modify the remaining object
+                Transform transform = gameObject.transform;
+                transform.localScale *= 1.2f; // Increase the size by 20%
+                // Change the sprite to a different sprite
+                Sprite newSprite = Resources.Load<Sprite>("fish_3"); // Change "NewSprite" to the name of your new sprite
+                GetComponent<SpriteRenderer>().sprite = newSprite;
+
+                Debug.Log("Species evolved");
+
+                // Destroy one of the collided objects
+                Destroy(collision.gameObject);
             }
-               
         }
-
-
     }
     void OnBecameInvisible()
     {
