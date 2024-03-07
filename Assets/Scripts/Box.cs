30779faa3c219b950618dc0023417c4b5bbc0d53
Atena Razm <atenarazm@gmail.com>
2024-03-06 17:05:13 -0800

    species land on the Box's Bottom, needs Left and Right stability, Plus Objects passing through the Ground

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+
+   void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("bear hit BOX");
+    }
+}
