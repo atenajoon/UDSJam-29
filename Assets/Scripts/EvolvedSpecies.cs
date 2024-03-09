59dc493fa757c5da94446c7b7cb77dcb5ea67720
Atena Razm <atenarazm@gmail.com>
2024-03-09 10:51:22 -0800

    on collision of similar tags, name of them are added to a list, and they get destroyed. In the update method, we can see that on every collision 2 exact same names are added to the list.

--- Assets/Scripts/EvolvedSpecies.cs
+++ Assets/Scripts/EvolvedSpecies.cs
@@ -5,8 +5,21 @@ using UnityEngine;
 public class EvolvedSpecies : MonoBehaviour
 {
     // List to store collided objects
-    private List<GameObject> collidedObjects = new List<GameObject>();
+    private List<string> collidedObjects = new List<string>();
 
+
+    void Update()
+    {
+        // Check if the list is not empty before accessing its elements
+        if (collidedObjects.Count > 0)
+        {
+            // Print the name of the first element
+            Debug.Log("2_ Name of the First Element: " + collidedObjects[0]);
+
+            // Clear the list
+            // collidedObjects.Clear();
+        }
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         
@@ -16,15 +29,15 @@ public class EvolvedSpecies : MonoBehaviour
         // if 2 similar items collide
         if (speciesTag == collidedObjectTag)
         {
-            Debug.Log("similar");
-            collidedObjects.Add(collision.gameObject);
-            Destroy(gameObject);
+            string collidedObjectName = collision.gameObject.name;
+            // add the collision gameObject to the list
+            collidedObjects.Add(collidedObjectName);
+            // Debug.Log("1. list length: " + collidedObjects.Count);
+            Destroy(collision.gameObject);
+            Debug.Log("1_ collidedObjectName: " + collidedObjects[0]);
+
         }
 
-        // Example: Print the names of collided objects in the list
-        foreach (GameObject obj in collidedObjects)
-        {
-            Debug.Log("Collided Object: " + obj.name);
-        }
+    
     }
 }
