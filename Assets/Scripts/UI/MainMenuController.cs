08f52cf09f21e3661ed4dd5b16c873bcf15e4e96
gageracer <gageracer@gmail.com>
2024-03-09 20:42:51 -0800

    gameflow from start to finish

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -1,7 +1,9 @@
+using System.Threading;
 using UnityEngine;
-using UnityEngine.UI; // Required for interacting with UI elements like buttons
 using UnityEngine.SceneManagement;
-using UnityEngine.SocialPlatforms.Impl; // Required for changing scenes
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
+using Image = UnityEngine.UI.Image; // Required for changing scenes
 
 public class MainMenuController : MonoBehaviour
 {
@@ -10,6 +12,26 @@ public class MainMenuController : MonoBehaviour
     public Button creditsButton;
     public Button exitButton;
 
+    public Image bearImage;
+    public Sprite bearFirstSprite; // Reference to the final stage bear sprite set in the Inspector
+    public Sprite bearFinalSprite; // Reference to the final stage bear sprite set in the Inspector
+    public Image fishImage;
+    public Sprite fishFirstSprite; // Reference to the final stage bear sprite set in the Inspector
+    public Sprite fishFinalSprite; // Reference to the final stage bear sprite set in the Inspector
+    public Image rabbitImage;
+    public Sprite rabbitFirstSprite; // Reference to the final stage bear sprite set in the Inspector
+    public Sprite rabbitFinalSprite; // Reference to the final stage bear sprite set in the Inspector
+
+    public Image background;
+    public Sprite BackgroundSprite1;
+    public Sprite BackgroundSprite2;
+    public Sprite BackgroundSprite3;
+
+    private void Awake()
+    {
+        DisplayCreaturesBackground();
+
+    }
     private void Start()
     {
         // Add click listeners to the buttons
@@ -37,5 +59,76 @@ public class MainMenuController : MonoBehaviour
 
         // If you want to be able to quit from the Unity editor, use the following line:
         // UnityEditor.EditorApplication.isPlaying = false;
+    }
+    // Somewhere in your menu scene script
+    void DisplayCreaturesBackground()
+    {
+        if (GameManager.Instance != null)
+        {
+
+
+            if (GameManager.Instance.scoreBoard["Bear"])
+            {
+                // Logic to display the bear image
+                bearImage.sprite = bearFinalSprite;
+                bearImage.color = Color.white;
+                Debug.Log("scroreBOard bear:" + GameManager.Instance.scoreBoard["Bear"]);
+            }
+            else
+            {
+                bearImage.sprite = bearFirstSprite;
+            }
+
+            if (GameManager.Instance.scoreBoard["Fish"])
+            {
+                // Logic to display the fish image
+                fishImage.sprite = fishFinalSprite;
+                fishImage.color = Color.white;
+                Debug.Log("scroreBOard fish:" + GameManager.Instance.scoreBoard["Fish"]);
+            }
+            else
+            {
+                fishImage.sprite = fishFirstSprite;
+            }
+
+            if (GameManager.Instance.scoreBoard["Rabbit"])
+            {
+                Debug.Log("scroreBOard rab:" + GameManager.Instance.scoreBoard["Rabbit"]);
+                // Logic to display the rabbit image
+                rabbitImage.sprite = rabbitFinalSprite;
+                rabbitImage.color = Color.white;
+            }
+            else
+            {
+                rabbitImage.sprite = rabbitFirstSprite;
+            }
+
+            int collectedCount = CountCollectedFinalStages();
+            Debug.Log("scroreBOard count:" + collectedCount);
+            // Assuming you have different backgrounds for different counts
+            background.sprite = collectedCount switch
+            {
+                1 => BackgroundSprite2,
+                2 => BackgroundSprite3,
+                3 => BackgroundSprite3,
+                _ => BackgroundSprite3,
+            };
+        }
+        else
+        {
+            Invoke(nameof(DisplayCreaturesBackground), 0.1f); // A slight delay to ensure GameManager is ready
+        }
+    }
+    public int CountCollectedFinalStages()
+    {
+        int count = 0;
+        foreach (var isCollected in GameManager.Instance.scoreBoard.Values)
+        {
+            if (isCollected)
+            {
+                count++;
+            }
+        }
+        return count;
     }
 }
